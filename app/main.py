5ec1e9d192ebdb9970001dcf0b40499bde5576e8
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-12 17:38:59 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    file = command.split()
+    if len(file) != 3:
+        return
+    if file[0] != "cp":
+        return
+    fst_file = file[1]
+    snd_file = file[2]
+    if fst_file == snd_file:
+        return
+
+    try:
+        with open(fst_file, "r") as fd:
+            f = fd.read()
+        with open(snd_file, "w") as sn:
+            sn.write(f)
+    except FileNotFoundError:
+        return
