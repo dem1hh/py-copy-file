622cd1f244e77ca457c428304a27574fee1bcac6
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-12 17:42:47 +0300

    fix improve variable naming according to linter rules

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 def copy_file(command: str) -> None:
 
-    file = command.split()
-    if len(file) != 3:
+    parts_file = command.split()
+    if len(parts_file) != 3:
         return
-    if file[0] != "cp":
+    if parts_file[0] != "cp":
         return
-    fst_file = file[1]
-    snd_file = file[2]
+    fst_file = parts_file[1]
+    snd_file = parts_file[2]
     if fst_file == snd_file:
         return
 
     try:
         with open(fst_file, "r") as fd:
-            f = fd.read()
+            cop_file = fd.read()
         with open(snd_file, "w") as sn:
-            sn.write(f)
+            sn.write(cop_file)
     except FileNotFoundError:
         return
