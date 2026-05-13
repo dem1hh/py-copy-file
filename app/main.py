33158ca9b42ecd052c6923df7658c10672e4ce79
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-13 13:13:30 +0300

    fixed add user notifications about errors

--- app/main.py
+++ app/main.py
@@ -2,12 +2,15 @@ def copy_file(command: str) -> None:
 
     parts_file = command.split()
     if len(parts_file) != 3:
+        print("Incorrect count of arguments")
         return
     if parts_file[0] != "cp":
+        print("Excepted: cp")
         return
     fst_file = parts_file[1]
     snd_file = parts_file[2]
     if fst_file == snd_file:
+        print("Both files are the same")
         return
 
     try:
@@ -17,4 +20,5 @@ def copy_file(command: str) -> None:
         ):
             destination_file.write(source_file.read())
     except FileNotFoundError:
+        print("File {fst_file} not found")
         return
